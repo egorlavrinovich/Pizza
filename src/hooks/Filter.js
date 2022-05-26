ae41433bc788a9ab9a440faf4a897ec4ef0f16b8
Egor <egorlavrinovich@gmail.com>
2022-05-26 16:51:04 +0300

    fetching

--- src/hooks/Filter.js
+++ src/hooks/Filter.js
@@ -2,7 +2,7 @@ import { useEffect, useMemo } from 'react';
 const PopularPosts = (SearchItem,posts) => {
     const SortedPosts = useMemo(()=>{
     if (SearchItem) {
-    if (SearchItem === "price") {
+    if (SearchItem == "price") {
       return([...posts.sort((a, b) => a[SearchItem] - b[SearchItem])]);
     } else {
       return([...posts.sort((a, b) => b[SearchItem] - a[SearchItem])]); // ??? Допилить
@@ -12,10 +12,10 @@ const PopularPosts = (SearchItem,posts) => {
 return SortedPosts
 };
 
-export const UseSortedPosts = (SearchCategory,SearchItem,posts) => { // Посмотреть есть нюанс если НЕ кликаешься на Всё, то не работает сортировка по популярности
-    const result = PopularPosts(SearchItem,posts)
+export const UseSortedPosts = (SearchCategory="all",SearchItem,posts) => { // Посмотреть есть нюанс если НЕ кликаешься на Всё, то не работает сортировка по популярности
+  const result = PopularPosts(SearchItem,posts)
     const ChoosedCategory = useMemo(()=>{
-    if (SearchCategory||SearchItem) {
+    if (SearchCategory) {
           if (SearchCategory == "all") {
             return result
           } else {
