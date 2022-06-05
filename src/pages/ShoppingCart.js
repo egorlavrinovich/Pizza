db6322502b5a3542bc097e22e4d649385dd111ea
Egorlaptop <egorlavrinovich@gmail.com>
2022-06-05 15:12:02 +0300

    Adaptiv pizza

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -2,12 +2,9 @@ import React from "react";
 import Header from "../components/header/header";
 import { useSelector, useDispatch } from "react-redux";
 import "../scss/app.scss";
-import EmptyShopcart from "../assets/img/empty-cart.png";
-import trash from "../assets/img/trash.svg";
 import Svg from "../components/UI/svg/svg";
 const ShoppingCart = () => {
   const OrderedPizzes = useSelector((state) => state.shopcart.pizzas);
-  console.log(OrderedPizzes);
   return (
     <div className="wrapper">
       <Header />
@@ -18,17 +15,29 @@ const ShoppingCart = () => {
               <li key={item.id}>
                 <img src={item.url} />
                 <div className="order">
-                  <span>{`Пицца: ${item.name}`}</span>
+                  <span className="title">{`Пицца: ${item.name}`}</span>
                   <span>{`Длина: ${item.length} см`}</span>
                   <span>{`Тесто: ${item.dough}`}</span>
-                  <span>{`Содержание мяса: ${item.meet ? "да" : "нет"}`}</span>
-                  <span>{`Содержание грибов: ${
+                  <span>{`Наличие мяса: ${item.meet ? "да" : "нет"}`}</span>
+                  <span>{`Наличие грибов: ${
                     item.mashrooms ? "да" : "нет"
                   }`}</span>
                   <span>{`Отсрая: ${item.peperoni ? "да" : "нет"}`}</span>
+                  <div className="shoppricemobile">
+                    {`Цена: ${item.price} ₽`}
+                    <div className="counter">+</div>
+                    <div className="trash">
+                      <Svg svg="trash" />
+                    </div>
+                  </div>
                 </div>
-                <div className="actionpart">
-                  <div className="shopprice">{`Цена: ${item.price}`}</div>
+                <div className="shopprice">
+                  {`Цена: ${item.price} ₽`}
+                  <div className="counter">
+                    <Svg svg="minus" />
+                    {}
+                    <Svg svg="plus" />
+                  </div>
                   <div className="trash">
                     <Svg svg="trash" />
                   </div>
