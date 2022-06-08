abc9c51bf8d9576b0a17ab71b562f483f490b903
Egor <egorlavrinovich@gmail.com>
2022-06-08 11:32:48 +0300

    Decomposition Корзина товаров

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,14 +1,8 @@
 import React from "react";
 import Header from "../components/header/header";
-import { useSelector, useDispatch } from "react-redux";
 import "../scss/app.scss";
-import Svg from "../components/UI/svg/svg";
-import { AddPizza, DeletePizza, ReducePizza } from "../redux/Slice/Price";
 import PizzaShopcart from '../components/Pizza-shopcart-block/PizzaShopcart';
 const ShoppingCart = () => {
-  const OrderedPizzes = useSelector((state) => state.shopcart.pizzas);
-  const dispatch = useDispatch();
-  const AvailablePiezzes = useSelector((state) => state.shopcart.counterPizzes);
   return (
     <div className="wrapper">
       <Header />
