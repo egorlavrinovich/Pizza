a34a37668080dfd23a4ba7b415567de063a98a6c
Egor <egorlavrinovich@gmail.com>
2022-06-08 11:31:40 +0300

    Decomposition

---
+++ src/components/Pizza-shopcart-block/PizzaShopcart.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import PizzaShopCartItem from './PizzaShopcartItem.jsx/PizzaShopCartItem';
+const PizzaShopcart = () => {
+    const OrderedPizzes = useSelector((state) => state.shopcart.pizzas);
+
+    return (
+        <div className="shoppingcart">
+        {OrderedPizzes.length &&
+          OrderedPizzes.map((item, index) => (
+            <PizzaShopCartItem item={item} index={index}/>
+          ))}{" "}
+      </div>
+    );
+};
+
+export default PizzaShopcart;
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import "../scss/app.scss";
 import Svg from "../components/UI/svg/svg";
 import { AddPizza, DeletePizza, ReducePizza } from "../redux/Slice/Price";
+import PizzaShopcart from '../components/Pizza-shopcart-block/PizzaShopcart';
 const ShoppingCart = () => {
   const OrderedPizzes = useSelector((state) => state.shopcart.pizzas);
   const dispatch = useDispatch();
@@ -11,65 +12,7 @@ const ShoppingCart = () => {
   return (
     <div className="wrapper">
       <Header />
-      <div className="shoppingcart">
-        {OrderedPizzes.length &&
-          OrderedPizzes.map((item, index) => (
-            <ul key={index}>
-              <li>
-                <img src={item.url} />
-                <div className="order">
-                  <span className="title">{`Пицца: ${item.name}`}</span>
-                  <span>{`Длина: ${item.length} см`}</span>
-                  <span>{`Тесто: ${item.dough}`}</span>
-                  <span>{`Наличие мяса: ${item.meet ? "да" : "нет"}`}</span>
-                  <span>{`Наличие грибов: ${
-                    item.mashrooms ? "да" : "нет"
-                  }`}</span>
-                  <span>{`Острая: ${item.peperoni ? "да" : "нет"}`}</span>
-                  <div className="shoppricemobile">
-                    {`Цена: ${item.price} ₽`}
-                    <div className="counter">
-                      <div className="minus">
-                        <Svg svg="minus" />
-                      </div>
-                      <div className="count">1</div>
-                      <div className="plus">
-                        <Svg svg="plus" />
-                      </div>
-                      <div className="trash">
-                        <Svg svg="trash" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="shopprice">
-                  {`Цена: ${item.price} ₽`}
-                  <div className="counter">
-                    <div
-                      className="minus"
-                      onClick={() => dispatch(ReducePizza(item))}
-                    >
-                      <Svg svg="minus" />
-                    </div>
-                    <div className="count">{AvailablePiezzes[item.id]}</div>
-                    <div
-                      className="plus"
-                      onClick={() => dispatch(AddPizza(item))}
-                    >
-                      <Svg svg="plus" />
-                    </div>
-                  </div>
-                  <div
-                    className="trash"
-                    onClick={() => dispatch(DeletePizza(item))}
-                  >
-                    <Svg svg="trash" />
-                  </div>
-                </div>
-              </li>
-            </ul>
-          ))}{" "}
-      </div>
+      <PizzaShopcart/>
     </div>
   );
 };
