72645673b86ef1b44156d11847744cdada218026
Egor <egorlavrinovich@gmail.com>
2022-06-16 14:11:20 +0300

    ss

--- src/redux/Slice/Price.js
+++ src/redux/Slice/Price.js
@@ -6,6 +6,7 @@ const initialState = {
   price: 0,
   count: 0,
   pizzas: [],
+  allpizzas:[],
   counterPizzes: {},
 };
 
@@ -14,37 +15,37 @@ export const ShoppingCart = createSlice({
   initialState,
   reducers: {
     AddPizza(state, action) {
-      const valueconsist = state.pizzas.filter(
-        (item) => JSON.stringify(action.payload) === JSON.stringify(item)
-      );
-      if (valueconsist.length) {
-        if (state.counterPizzes[action.payload.id]) {
-          state.counterPizzes[action.payload.id] += 1;
-        } else {
-          state.counterPizzes[action.payload.id] = 2;
+      const obj = {...action.payload,count:1}
+      function findpizza(pizza) {
+        const pizas = state.allpizzas.filter(item=>JSON.stringify(item)===JSON.stringify(pizza))
+        if(pizas.length){
+          return pizas.length
         }
-      }
-      if (!state.counterPizzes[action.payload.id]) {
-        state.pizzas.push(action.payload);
+        return pizza
+      } 
+      if(findpizza(action.payload)){
+        state.pizzas.push(obj)
+        state.allpizzas.push(action.payload)
       }
       state.price += action.payload.price;
       state.count += 1;
+      console.log(current(state))
     },
     DeletePizza(state, action) {
-      const count = state.counterPizzes[action.payload.id]; // Обязательно записываем наш стейт в переменную
-      state.count -= count;
-      state.price -= +count * +action.payload.price;
-      delete state.counterPizzes[action.payload.id];
-      state.pizzas = state.pizzas.filter(
-        (item) => JSON.stringify(item) !== JSON.stringify(action.payload)
-      );
+      // const count = state.counterPizzes[action.payload.id]; // Обязательно записываем наш стейт в переменную
+      // state.count -= count;
+      // state.price -= +count * +action.payload.price;
+      // delete state.counterPizzes[action.payload.id];
+      // state.pizzas = state.pizzas.filter(
+      //   (item) => JSON.stringify(item) !== JSON.stringify(action.payload)
+      // );
     },
     ReducePizza(state, action) {
-      if (state.counterPizzes[action.payload.id] > 1) {
-        state.counterPizzes[action.payload.id] -= 1;
-        state.count -= 1;
-        state.price -= action.payload.price;
-      }
+      // if (state.counterPizzes[action.payload.id] > 1) {
+      //   state.counterPizzes[action.payload.id] -= 1;
+      //   state.count -= 1;
+      //   state.price -= action.payload.price;
+      // }
     },
   },
 });
